e15162daca3a6a54c84f5665ebb67f938cb8aa01
joaolfp <joaolucasfp2001@gmail.com>
2024-05-31 02:12:38 -0300

    Update swiftfiletools to 1.3.1

--- swiftfiletools.rb
+++ swiftfiletools.rb
@@ -1,12 +1,11 @@
 class Swiftfiletools < Formula
     desc "SwiftFileTools is a tool for you to generate Colors, Constants, ViewCode and files for SwiftGen."
     homepage "https://github.com/heroesofcode/SwiftFileTools"
-    url "https://github.com/heroesofcode/SwiftFileTools/releases/download/1.3.0/SwiftFileTools.tar.gz"
-    sha256 "0b111ed9cb7385f765a05e7c729a38baba592ac789ee4abb6efe573c422aeb5f"
+    url "https://github.com/heroesofcode/SwiftFileTools/releases/download/1.3.1/SwiftFileTools.tar.gz"
+    sha256 "81b8ab53b5958f9d7ec039ce0509fc57f832220f082cb2256fc9fe50b1d9a242"
   
     def install
-        system "cd SwiftFileTools && swift build --disable-sandbox -c release"
-        bin.install "SwiftFileTools/.build/release/SwiftFileTools"
+        bin.install "SwiftFileTools"
     end
-  end
+end
   
