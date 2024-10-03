8e182b30c5c5e31dcad9ccd2bdf6cc3c276a679d
João Lucas <joaolucasfp2001@gmail.com>
2024-10-02 22:11:17 -0300

    Update version xrun to 0.11.0

--- xrun.rb
+++ xrun.rb
@@ -1,10 +1,10 @@
 class Xrun < Formula
-    desc "Command-line tools for macOS. With xrun you can run iOS and iPadOS unit tests through the terminal or CI with more ease and reading."
-    homepage "https://github.com/heroesofcode/xrun"
-    url "https://github.com/heroesofcode/xrun/releases/download/0.10.0/xrun.tar.gz"
-    sha256 "c469b44aa83dd6d78f4e29ecd40e5e69116b98f7d7107792d7ee2a27107e5372"
-  
-    def install
-      bin.install "xrun"
-    end
+  desc "Command-line tools for macOS. With xrun you can run iOS and iPadOS unit tests through the terminal or CI with more ease and reading."
+  homepage "https://github.com/heroesofcode/xrun"
+  url "https://github.com/heroesofcode/xrun/releases/download/0.11.0/xrun.tar.gz"
+  sha256 "bd1a4add08d51049650166c5ebdd9501ebb3265b3d8a43189c01bd5b4ac6a3f7"
+
+  def install
+    bin.install "xrun"
   end
+end
