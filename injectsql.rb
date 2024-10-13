874a0274136a6da1e44730df0e17e087e9530ea2
João Lucas <joaolucasfp2001@gmail.com>
2024-10-13 13:58:22 -0300

    Fix url injectsql

--- injectsql.rb
+++ injectsql.rb
@@ -1,7 +1,7 @@
 class Injectsql < Formula
   desc "Command Line Tools to check for SQL Injection vulnerability."
   homepage "https://github.com/heroesofcode/inject-sql"
-  url "https://github.com/heroesofcode/inject-sql/releases/download/0.1.0/injectql.tar.gz"
+  url "https://github.com/heroesofcode/inject-sql/releases/download/0.1.0/injectsql.tar.gz"
   sha256 "e13c54b2cec4a173e9e40affab5342e1c1c566a0fe0adc885234a19e8a2919c7"
 
   def install
