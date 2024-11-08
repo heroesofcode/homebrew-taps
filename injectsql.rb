e5109ddd4cb0d0d3b46b4d6133e54d7aae2bf617
João Lucas <joaolucasfp2001@gmail.com>
2024-11-08 00:36:59 -0300

    Update injectsql version to 0.5.0

--- injectsql.rb
+++ injectsql.rb
@@ -1,8 +1,8 @@
 class Injectsql < Formula
   desc "Command Line Tools to check for SQL Injection vulnerability."
   homepage "https://github.com/heroesofcode/inject-sql"
-  url "https://github.com/heroesofcode/inject-sql/releases/download/0.4.0/injectsql.tar.gz"
-  sha256 "7405b17484c0193047fcf6951631920dbd77ab60b93281383dbdb95cb8f5649f"
+  url "https://github.com/heroesofcode/inject-sql/releases/download/0.5.0/injectsql.tar.gz"
+  sha256 "21d7eba868de255c14ec310b70e01066fcaa4d909829502839701d6928641555"
 
   def install
     bin.install "injectsql"
