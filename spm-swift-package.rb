49b0ddf0144d92ef81a59df9ec0aa0febcbf698c
João Lucas <joaolucasfp2001@gmail.com>
2025-12-09 00:57:57 -0300

    Fix shasum spm-swift-package

--- spm-swift-package.rb
+++ spm-swift-package.rb
@@ -2,7 +2,7 @@ class SpmSwiftPackage < Formula
     desc "Command Line Tools for macOS to create project in Swift Package Manager with desirable files."
     homepage "https://github.com/heroesofcode/spm-swift-package"
     url "https://github.com/heroesofcode/spm-swift-package/releases/download/v0.10.0/spm-swift-package.tar.gz"
-    sha256 "99510fa81b4ae361d765f0f5103faf9acbc7885917831f281bdc69c3617cd990"
+    sha256 "ae85b0b3ae6040771b9aaa39dab2b123ac19f32be1c056953b2e654de4ff6753"
   
     def install
       bin.install "spm-swift-package"
