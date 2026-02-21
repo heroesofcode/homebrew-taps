98fe1902feff5c48406d98e4da6dbbb57d2fafb0
João Lucas <joaolucasfp2001@gmail.com>
2026-02-21 16:13:58 -0300

    Update spm-swift-package to v0.13.0

--- spm-swift-package.rb
+++ spm-swift-package.rb
@@ -1,8 +1,8 @@
 class SpmSwiftPackage < Formula
     desc "Command Line Tools for macOS to create project in Swift Package Manager with desirable files."
     homepage "https://github.com/heroesofcode/spm-swift-package"
-    url "https://github.com/heroesofcode/spm-swift-package/releases/download/v0.12.0/spm-swift-package.tar.gz"
-    sha256 "9ef1d631d09a1734314349ef6b7fcb7c7993e1fa8b7bce9d3e29880979807dca"
+    url "https://github.com/heroesofcode/spm-swift-package/releases/download/v0.13.0/spm-swift-package.tar.gz"
+    sha256 "218e34b3bcfac4b7f7e71dd2d51b4fd9b5f7f848266c33b3fffe0823a0199438"
   
     def install
       bin.install "spm-swift-package"
